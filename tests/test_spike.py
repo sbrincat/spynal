4adf148e508d31c80d8ad25cf3abd263c379a6e3
jtauber <john.tauber@gmail.com>
2020-09-24 12:48:04 -0400

    add dummy test

--- tests/test_spike.py
+++ tests/test_spike.py
@@ -1,1 +1,8 @@
 "Tests for spike_analysis.py will go here"
+from unittest.mock import Mock
+from .. import spike_analysis
+# from .. import spike_analysis
+
+def test_bin_count_return_shape():
+    x = 5
+    assert x == 5
