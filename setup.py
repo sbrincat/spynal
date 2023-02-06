0bcc6553451b96dc15b3c6ed46d2e72970e69ac7
sbrincat <scott.brincat@gmail.com>
2023-02-06 11:45:50 -0500

    Updated version in setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setuptools.setup(
       name='spynal',
-      version='0.1.1',
+      version='0.1.2',
       description='Simple Python Neural Analysis Library',
       long_description=long_description,
       long_description_content_type='text/markdown',
